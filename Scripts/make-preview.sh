5f09c5de537806b61080bec4b93ae449eff46c2b
Mario Correa <mariomcorrea3@gmail.com>
2026-08-27 16:00:12 -0300

    Merge pull request #28 from sensitiky/fix/013-macos-deployment-target
    fix(013-macos-deployment-target): lower deployment target to macOS 15 and allow Intel

--- Scripts/make-preview.sh
+++ Scripts/make-preview.sh
@@ -48,7 +48,7 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>CFBundlePackageType</key><string>APPL</string>
     <key>CFBundleShortVersionString</key><string>0.1.0</string>
     <key>CFBundleVersion</key><string>1</string>
-    <key>LSMinimumSystemVersion</key><string>26.0</string>
+    <key>LSMinimumSystemVersion</key><string>15.0</string>
     <key>NSPrincipalClass</key><string>NSApplication</string>
     <key>NSHighResolutionCapable</key><true/>
     <key>CFBundleIconFile</key><string>Zero.icns</string>
